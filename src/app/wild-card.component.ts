4ed5fe309aaa966f892b10449e53f32b3c8a53a2
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-07 00:14:01 +0800

    Initial commit

---
+++ src/app/wild-card.component.ts
@@ -0,0 +1,38 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-wild-card',
+  standalone: true,
+  template: `
+    <h1>Angular Activated Route</h1>
+    <span>URL: {{ url() }}</span>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class WildCardComponent {
+  private route = signal<ActivatedRoute>(inject(ActivatedRoute));
+  private route$ = toObservable(this.route);
+  protected url = computed(() => {
+    return this.route()
+      .snapshot.url.map((segment) => segment.path)
+      .join('/');
+  });
+
+  constructor() {
+    this.route$.subscribe((route) => {
+      console.log('route', route);
+    });
+    effect(() => {
+      console.log('url:', this.url());
+    });
+  }
+}
